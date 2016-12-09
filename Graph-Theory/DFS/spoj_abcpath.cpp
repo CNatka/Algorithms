70275e7ce33c4ce6798ed7b5b1f83c849c0ed85c
Sajal Sarkar <ssstarsarkar664@gmail.com>
2016-12-09 12:34:32 +0530

    Update spoj_abcpath.cpp

--- Graph-Theory/DFS/spoj_abcpath.cpp
+++ Graph-Theory/DFS/spoj_abcpath.cpp
@@ -1,15 +1,15 @@
 /*-------------------------------------------------------------------------------------------
-*	Name 			      : CNatka
+*	Name 			: CNatka
 *	Problem 	    	: ABCPATH
-* Source  		    : Spoj
-*	Link			      : http://www.spoj.com/problems/ABCPATH/
-*	DS				      : Graph
-*	Algo			      : DFS
-*	Date			      : Dec 09, 2016
-*	Complexityy		  : O(H+W) || AC(0.00 sec)  		
-*	Solved			    : self
-*	Alternative Sol	: NA
-*	Note 			      : This code should be used for the purpose of learning only, use it at your own risk	
+* 	Source  		: Spoj
+*	Link			: http://www.spoj.com/problems/ABCPATH/
+*	DS			: Graph
+*	Algo			: DFS
+*	Date			: Dec 09, 2016
+*	Complexityy		: O(H+W) || AC(0.00 sec)  		
+*	Solved			: self
+*	Alternative Sol		: NA
+*	Note 			: This code should be used for the purpose of learning only, use it at your own risk	
 ---------------------------------------------------------------------------------------------*/
 
 #include <cstdio>
