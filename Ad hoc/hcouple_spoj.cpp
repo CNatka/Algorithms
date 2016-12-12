b6ae6f9cd47c386a34f60a3dd8410c9404a6dec3
Sajal Sarkar <ssstarsarkar664@gmail.com>
2016-12-12 18:43:39 +0530

    Create hcouple_spoj.cpp

---
+++ Ad hoc/hcouple_spoj.cpp
@@ -0,0 +1,36 @@
+/*--------------------------------------------------------------------------------------------------------
+*	Name			:	CNatka
+*	Problem			:	HCOUPLE
+*	Source			:	Spoj
+*	Link			:	http://www.spoj.com/problems/HCOUPLE/
+*	DS				:	NA
+*	Algo			:	NA
+*	Date			:	Dec 12, 2016
+*	Complexity		:	O(1)
+*	Solved			:	self
+*	Alternative Sol	:	NA
+*	Note			:	This code should be used for the purpose of learning only, use it at your own risk
+*----------------------------------------------------------------------------------------------------------*/
+
+#include <cstdio>
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	cin.clear(); cin.ignore(1,'\n');
+	for(int j=1;j<=T;j++)
+	{
+		string s;
+		getline(cin, s);
+		int count=0;
+		int n = s.size();
+		for(int i=0;i<n;i++)
+			count += s[i];
+		if(count%3==0)	printf("Case %d: Yes\n",j);
+		else			printf("Case %d: No\n",j);
+	}
+	return 0;
+}
