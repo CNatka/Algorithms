6748750899e3dc5c2df3e19d110be92b8a5b5260
Sajal Sarkar <ssstarsarkar664@gmail.com>
2017-01-24 15:21:32 +0530

    Create codechef_lisdigit.cpp

---
+++ Ad hoc/codechef_lisdigit.cpp
@@ -0,0 +1,43 @@
+/*--------------------------------------------------------------------------------------------------------
+*	Name			:	CNatka
+*	Problem			:	LISDIGIT
+*	Source			:	Codechef
+*	Link			:	https://www.codechef.com/problems/LISDIGIT
+*	DS				:	NA
+*	Algo			:	NA
+*	Date			:	Jan 22, 2017
+*	Complexity		:	O(T*n)	||	AC(0.04 sec)
+*	Solved			:	self
+*	Alternative Sol	:	NA
+*	Note			:	This code should be used for the purpose of learning only, use it at your own risk
+*----------------------------------------------------------------------------------------------------------*/
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+		int n;
+		scanf("%d",&n);
+		vector<int>V;
+		for(int i=0; i<n; i++)
+		{
+			int temp;
+			scanf("%d",&temp);
+			V.push_back(temp);
+		}
+		long long num = V[0];
+		for(int i=1; i<n; i++)
+		{
+			num *= 10;
+			num += V[i];
+		}
+		printf("%lld\n",num);
+	}
+	return 0;
+}
